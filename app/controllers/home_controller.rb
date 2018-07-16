66d32dcd068e1ad1f2a9407e017f60c9a042ddce
gyeol1212 <gyeol1212@likelion.org>
2018-07-16 14:41:15 +0900

    START

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def members
+  end
+
+  def recruit
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :posts
+  get 'home/index'
+
+  get 'home/about'
+
+  get 'home/members'
+
+  get 'home/recruit'
+
+  root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
