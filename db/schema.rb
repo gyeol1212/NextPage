cf4bdc75aa0a48cc9558f47e2e324ada35ddfe53
qlqnf16 <lee.jeongmin1027@likelion.org>
2018-07-16 21:08:51 +0900

    container 삭제

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,9 @@ ActiveRecord::Schema.define(version: 20180716100613) do
   create_table "comments", force: :cascade do |t|
     t.string   "content"
     t.integer  "post_id"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "members", force: :cascade do |t|
@@ -33,16 +31,13 @@ ActiveRecord::Schema.define(version: 20180716100613) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.string   "name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
