268b5d04fbafd1a71b62c2a21804179d5887afc9
qlqnf16 <lee.jeongmin1027@likelion.org>
2018-07-16 23:11:00 +0900

    login signup

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,11 @@ ActiveRecord::Schema.define(version: 20180716100613) do
   create_table "comments", force: :cascade do |t|
     t.string   "content"
     t.integer  "post_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "members", force: :cascade do |t|
@@ -38,6 +40,7 @@ ActiveRecord::Schema.define(version: 20180716100613) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
