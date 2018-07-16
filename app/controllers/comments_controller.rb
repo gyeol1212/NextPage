a674c482180a3c964ad07b0f77e02620a247336b
staycozyboy <dkc984@likelion.org>
2018-07-16 17:25:23 +0900

    Model Realtion 작성

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment=Comment.new(comment_params)
     @comment.post_id = params[:post_id]
+    @comment.user_id = current_user.id
     @comment.save
 
     redirect_to :back
